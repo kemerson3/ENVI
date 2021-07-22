cd4897700f970086391b3fb1b49153729b28e10b
kemerson3 <emersonk@duq.edu>
2021-07-22 14:44:09 -0400

    updated with results

--- T-Test/T-Test.r
+++ T-Test/T-Test.r
@@ -6,3 +6,13 @@ hist(x1)
 hist(x2)
 
 t.test(x1, x2)
+
+#results
+# data:  x1 and x2
+# t = 44.331, df = 1996.2, p-value < 2.2e-16
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   1.907971 2.084599
+# sample estimates:
+#   mean of x  mean of y 
+# 1.0210487 -0.9752365 
